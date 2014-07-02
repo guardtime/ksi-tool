6c2c9c4dfa4a93c640acb2f61440470161b9064a
taavi.valjaots <taavi.valjaots@guardtime.com>
2014-07-02 12:20:08 +0300

    win buildscript updated

--- WinBuild.bat
+++ WinBuild.bat
@@ -1,12 +1,10 @@
 @ECHO OFF
 CALL "%ProgramW6432%\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
 
-REM SET CURL_DIR=C:\Users\Taavi\Documents\GuardTime\TEST_OLD_LIB\curl-7.37.0\builds\libcurl-vc10-x64-debug-static-ipv6-sspi-spnego-winssl
-REM SET CURL_DIR="C:\Users\Taavi\Documents\GuardTime\TEST_OLD_LIB\curl-7.37.0\builds\libcurl-vc10-x64-release-static-ipv6-sspi-spnego-winssl"
-REM SET OPENSSL_CA_FILE="C:\Users\Taavi\Documents\GuardTime\ksicapi\test\resource\tlv\mock.crt"
-REM SET OPENSSL_DIR="C:\Users\Taavi\Documents\GuardTime\TEST_OLD_LIB\openssl-0.9.8g-bin-win64" 
-REM SET KSI_INCLUDE_DIR = "C:\Users\Taavi\Documents\GuardTime\ksicapi\src\ksi"
-REM CURL_DIR=$(CURL_DIR) OPENSSL_CA_FILE=$(OPENSSL_CA_FILE) OPENSSL_DIR=$(OPENSSL_DIR) KSI_INCLUDE_DIR=$(KSI_INCLUDE_DIR)
+SET OPENSSL_DIR=C:\Users\Taavi\Documents\GuardTime\LIBS\openssl-0.9.8g-win64
+SET CURL_DIR=C:\Users\Taavi\Documents\GuardTime\LIBS\curl-7.37.0
+SET OPENSSL_CA_FILE=C:\Users\Taavi\Documents\GuardTime\ksicapi\test\resource\tlv\mock.crt
+SET KSI_DIR=C:\Users\Taavi\Documents\GuardTime\ksicapi\out
 
 
 ECHO ************ Rebuilding project ************
