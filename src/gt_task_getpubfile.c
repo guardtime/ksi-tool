b5e7f1b9975a4a5b9f2305f6b8069d97e5543f21
taavi.valjaots <taavi.valjaots@guardtime.com>
2014-07-07 14:37:06 +0300

    Publications file download refcatoring

--- src/gt_task_getpubfile.c
+++ src/gt_task_getpubfile.c
@@ -5,8 +5,10 @@ int GT_getPublicationsFileTask(GT_CmdParameters *cmdparam, GT_Tasks task){
 	KSI_CTX *ksi = NULL;
 	int res;
 	KSI_PublicationsFile *publicationsFile = NULL;
-	const char *fileName = NULL;
-
+        unsigned int count;
+        char *raw=NULL;
+        int raw_len=0;
+	FILE *out = NULL;
 
         res = KSI_global_init();
         ERROR_HANDLING("Unable to init KSI global resources.\n");
@@ -21,13 +23,29 @@ int GT_getPublicationsFileTask(GT_CmdParameters *cmdparam, GT_Tasks task){
 	res = KSI_verifyPublicationsFile(ksi, publicationsFile);
         ERROR_HANDLING_STATUS_DUMP("failed! Unable to verify publications file.\n");
 	printf("ok.\n");
+        res = KSI_PublicationsFile_serialize(ksi, publicationsFile, &raw, &raw_len);
+        ERROR_HANDLING_STATUS_DUMP("Unable serialize publications file.\n");
 
-        res = KSI_PublicationsFile_toFile(ksi, publicationsFile, cmdparam->outPubFileName);
-        ERROR_HANDLING_STATUS_DUMP("Unable to save publications file.\n");
+       /* Open output file. */
+        out = fopen(cmdparam->outPubFileName, "wb");
+	if (out == NULL) {
+            fprintf(stderr, "Unable to ope publications file '%s' for writing.\n", cmdparam->outPubFileName);
+            res = KSI_IO_ERROR;
+            goto cleanup;
+	}
         
-        printf("Publications file %s saved.\n", cmdparam->outPubFileName);
+        /* Write output file */
+        count = fwrite(raw, 1, raw_len, out);
+	if (count != raw_len) {
+            fprintf(stderr, "Unable to write publications file '%s'.\n", cmdparam->outPubFileName);
+            res = KSI_IO_ERROR;
+            goto cleanup;
+	}
+        
+        printf("Publications file '%s' saved.\n", cmdparam->outPubFileName);
         
     cleanup:
+    	if (out != NULL) fclose(out);
         KSI_CTX_free(ksi);
 	KSI_global_cleanup();
         return res;
