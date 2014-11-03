6dbc01f76dc661ae6dc90056c229c84972392f37
taavi.valjaots <taavi.valjaots@guardtime.com>
2014-11-03 19:07:49 +0200

    missig file added

---
+++ src/gt_task_other.c
@@ -0,0 +1,152 @@
+#include "gt_task_support.h"
+#include "try-catch.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+
+static void printSignaturesRootHash_and_Time(const KSI_Signature *sig);
+static void setSystemTime_throws(const KSI_Signature *sig);
+
+bool GT_other(Task *task){
+	KSI_CTX *ksi = NULL;
+	KSI_DataHasher *hsr = NULL;
+	KSI_DataHash *hsh = NULL;
+	KSI_Signature *sig = NULL;
+	bool state = true;
+	
+	bool n,r, d,t;
+	
+	n = paramSet_isSetByName(task->set, "n");
+	r = paramSet_isSetByName(task->set, "r");
+	d = paramSet_isSetByName(task->set, "d");
+	t = paramSet_isSetByName(task->set, "t");
+	
+	resetExeptionHandler();
+	try
+		CODE{
+			/*Initalization of KSI */
+			initTask_throws(task, &ksi);
+
+			if (task->id == getRootH_T || task->id == setSysTime) {
+				KSI_DataHasher_open_throws(ksi, KSI_getHashAlgorithmByName("default"), &hsr);
+				KSI_DataHasher_add_throws(hsr, "tere",4);
+				KSI_DataHasher_close_throws(hsr, &hsh);
+				KSI_Signature_create_throws(ksi, hsh, &sig);
+				
+				printf("MIS TOIMUB?\n");
+				if (task->id == getRootH_T) {
+					printSignaturesRootHash_and_Time(sig);
+				}
+				else if (task->id == setSysTime) {
+					setSystemTime_throws(sig);
+				}
+				
+			}
+			
+		}
+		CATCH_ALL{
+			printf("failed.\n");
+			printErrorLocations();
+			exeptionSolved();
+			state = false;
+		}
+	end_try
+	
+	if(n) printf("\n");
+	if (n) printSignerIdentity(sig);
+	
+	KSI_DataHasher_free(hsr);
+	KSI_DataHash_free(hsh);
+	KSI_Signature_free(sig);
+	KSI_CTX_free(ksi);
+	
+	return state;
+}
+
+static void printSignaturesRootHash_and_Time(const KSI_Signature *sig){
+	int res;
+	KSI_CalendarAuthRec *calAuthrec = NULL;
+	KSI_PublicationData *pubData = NULL;
+	KSI_DataHash *hsh = NULL;
+	KSI_Integer *time = NULL;
+	char buf[256];
+	char strTime[1024];
+	time_t pubTm;
+	struct tm tm;		
+	
+	res = KSI_Signature_getCalendarAuthRec(sig, &calAuthrec);
+	if(res != KSI_OK || calAuthrec == NULL ) return;
+	
+	res = KSI_CalendarAuthRec_getPublishedData(calAuthrec, &pubData);
+	if(res != KSI_OK || pubData == NULL ) return;
+	
+	res = KSI_PublicationData_getImprint(pubData, &hsh);
+	if(res != KSI_OK || hsh == NULL ) return;
+	
+	res = KSI_PublicationData_getTime(pubData, &time);
+	if(res != KSI_OK || time == NULL ) return;
+	
+	if(KSI_DataHash_toString(hsh, buf, sizeof(buf)) != buf)
+		return;
+	
+	pubTm = (time_t)KSI_Integer_getUInt64(time);
+	gmtime_r(&pubTm, &tm);
+	strftime(strTime, sizeof(strTime), "%Y-%m-%d %H:%M:%S", &tm);
+	
+	printf("[%s]\n%s\n",strTime, buf+1);
+	
+	return;
+	}
+
+static void setSystemTime_throws(const KSI_Signature *sig){
+	int res;
+	KSI_CalendarAuthRec *calAuthrec = NULL;
+	KSI_PublicationData *pubData = NULL;
+	KSI_Integer *time = NULL;
+	time_t pubTm;
+	struct tm tm;		
+
+#ifdef _WIN32
+	SYSTEMTIME newTime;
+#endif	
+	
+	res = KSI_Signature_getCalendarAuthRec(sig, &calAuthrec);
+	if(res != KSI_OK || calAuthrec == NULL ) THROW_MSG(KSI_EXEPTION, "Unable to get calendar authentication record");
+
+	res = KSI_CalendarAuthRec_getPublishedData(calAuthrec, &pubData);
+	if(res != KSI_OK || pubData == NULL ) THROW_MSG(KSI_EXEPTION, "Unable to get published data");
+	
+	res = KSI_PublicationData_getTime(pubData, &time);
+	if(res != KSI_OK || time == NULL ) THROW_MSG(KSI_EXEPTION, "Unable to get time");
+	
+	pubTm = (time_t)KSI_Integer_getUInt64(time);
+	gmtime_r(&pubTm, &tm);
+	
+#ifdef _WIN32
+	newTime.wYear = 1900 + tm.tm_year;
+	newTime.wMonth = 1 +tm.tm_mon;
+	newTime.wDay = tm.tm_mday;
+	newTime.wHour = tm.tm_hour;
+	newTime.wMinute = tm.tm_min;
+	newTime.wSecond = tm.tm_sec;
+	newTime.wMilliseconds = 0;
+	
+	if(!SetSystemTime(&newTime)){
+		DWORD err = GetLastError();
+		if(err == ERROR_PRIVILEGE_NOT_HELD){
+			printf("Mis synnib\n");
+			THROW_MSG(NO_PRIVILEGES_EXEPTION, "User has no privileges to change date");
+		}
+		else{
+			THROW_MSG(NO_PRIVILEGES_EXEPTION, "Unable to set time");
+		}
+	}
+#else
+	struct timeval tv = {mktime(&tm), 0};
+    settimeofday(&tv, 0);
+#endif
+	return;
+}
