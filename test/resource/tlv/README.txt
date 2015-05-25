50bc695cd96cce29796aa05d739ae7cbf2f6b9e9
taavi.valjaots <taavi.valjaots@guardtime.com>
2015-05-26 01:07:44 +0300

    KSICAPI-213: Usless test data removed. Test directory changed.

--- test/resource/tlv/README.txt
+++
@@ -1,5 +0,0 @@
-TLV types:
-	0x01 	- 1..8 byte integer
-	0x02 	- string
-	0x03 	- Nested TLV8
-	0x1003	- Nested TLV16
--- test/run-tests.sh
+++ test/run-tests.sh
@@ -22,7 +22,7 @@
 dir=`dirname $0`
 tmp=${TMPDIR:-'test/out'}
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-tlv_dir="${SCRIPT_DIR}/resource/tlv"
+resource_dir="${SCRIPT_DIR}/resource"
 
 # Test Anything Protocol, from http://testanything.org/
 . ${dir}/tap-functions
@@ -36,7 +36,7 @@ echo \# ========================================================================
 echo \# Using $host as Guardtime Gateway Server. Specify custom server as 1st command-line argument.
 
 . ${dir}/endpoints.sh
-url_c="${tlv_dir}/mock.crt"
+url_c="${resource_dir}/mock.crt"
 
 echo \# Certificate: $url_c
 
@@ -61,11 +61,11 @@ okx src/ksitool -v -t -b ${tmp}/pub.bin -V $url_c
 diag "-------------------------------------------------------------------------------";
 
 diag "### Sign";
-okx src/ksitool -s -f ${SCRIPT_DIR}/testFile -o ${tmp}/tmp.ksig
+okx src/ksitool -s -f ${resource_dir}/testFile -o ${tmp}/tmp.ksig
 diag "------------------------------------------------------------------------------";
 
 diag "### Verify freshly created signature token";
-okx src/ksitool -v -b ${tmp}/pub.bin -i ${tmp}/tmp.ksig -f ${SCRIPT_DIR}/testFile -V $url_c
+okx src/ksitool -v -b ${tmp}/pub.bin -i ${tmp}/tmp.ksig -f ${resource_dir}/testFile -V $url_c
 diag "------------------------------------------------------------------------------";
 
 diag "###    Sign and verify raw hash using SHA-1"
@@ -73,7 +73,7 @@ okx src/ksitool -s -F SHA-1:${SH1_HASH} -o ${tmp}/sha1.ksig
 diag "------------------------------------------------------------------------------";
 
 diag "### Extending signature";
-okx src/ksitool -x -i ${tlv_dir}/ok-sig-2014-08-01.1.ksig -o ${tmp}/ext.ksig -V $url_c
+okx src/ksitool -x -i ${resource_dir}/ok-sig-2014-08-01.1.ksig -o ${tmp}/ext.ksig -V $url_c
 diag "------------------------------------------------------------------------------";
 
 diag "### Verifying extended signature";
@@ -94,13 +94,13 @@ diag "--------------------------------------------------------------------------
 like "`src/ksitool -x -i ${tmp}/tmp.ksig -o ${tmp}/ext.ksig -V $url_c 2>&1`" "There is no suitable publication yet" "Error extend no suitable publication"
 diag "------------------------------------------------------------------------------";
 
-like "`src/ksitool -x -i ${SCRIPT_DIR}/testFile -o ${tmp}/ext.ksig -V $url_c 2>&1`" "Unable to read signature from file." "Error extend not suitable format"
+like "`src/ksitool -x -i ${resource_dir}/testFile -o ${tmp}/ext.ksig -V $url_c 2>&1`" "Unable to read signature from file." "Error extend not suitable format"
 diag "------------------------------------------------------------------------------";
 
-like "`src/ksitool -x -i ${SCRIPT_DIR}/testFile -o ${tmp}/ext.ksig -V $url_c 2>&1`" "Unable to read signature from file." "Error verify not suitable format"
+like "`src/ksitool -x -i ${resource_dir}/testFile -o ${tmp}/ext.ksig -V $url_c 2>&1`" "Unable to read signature from file." "Error verify not suitable format"
 diag "------------------------------------------------------------------------------";
 
-like "`src/ksitool -v -x -i ${SCRIPT_DIR}/ok-sig-2014-04-30.1.ksig -f ${SCRIPT_DIR}/resource/TestData.txt -V $url_c 2>&1`" "Wrong document or signature" "Error verifying signature and wrong file"
+like "`src/ksitool -v -x -i ${resource_dir}/ok-sig-2014-04-30.1.ksig -f ${SCRIPT_DIR}/resource/TestData.txt -V $url_c 2>&1`" "Wrong document or signature" "Error verifying signature and wrong file"
 diag "------------------------------------------------------------------------------";
 
 like "`src/ksitool -s -F SHA-1:${SH1_HASH}FF -o ${tmp}/sha1.ksig 2>&1`" "Hash length is incorrect" "Error signing with SH1 and wrong hash"
@@ -109,13 +109,13 @@ diag "--------------------------------------------------------------------------
 like "`src/ksitool -s -F DUNNO:${SH1_HASH} -o ${tmp}/sha1.ksig 2>&1`" "Algorithm name is incorrect" "Error signing with unknown algorithm and wrong hash"
 diag "------------------------------------------------------------------------------";
 
-like "`src/ksitool -s -f ${SCRIPT_DIR}/testFile -o ${tmp}/tmp.ksig -S no_network_here 2>&1`" "no_network_here" "Error bad network provider"
+like "`src/ksitool -s -f ${resource_dir}/testFile -o ${tmp}/tmp.ksig -S no_network_here 2>&1`" "no_network_here" "Error bad network provider"
 diag "------------------------------------------------------------------------------";
 
 like "`src/ksitool -v -b ${tmp}/pub.bin -i ${tmp}/ext-t.ksig -f missing_file  2>&1`" "File does not exist" "Error Verify signature and missing file"
 diag "------------------------------------------------------------------------------";
 
-like "`src/ksitool -v -t -b ${SCRIPT_DIR}/testFile -V $url_c 2>&1`" "Unable to read publications file" "Error Invalid publications file"
+like "`src/ksitool -v -t -b ${resource_dir}/testFile -V $url_c 2>&1`" "Unable to read publications file" "Error Invalid publications file"
 diag "------------------------------------------------------------------------------";
 
 
