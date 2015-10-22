9bc1e9e3ceaa7626bf4242d24d629def59fb0546
taavi.valjaots <taavi.valjaots@guardtime.com>
2015-10-22 09:50:14 +0300

    Missing 'Copyright (C) [2015] Guardtime' added to ksitool_err and obj_printer c and h files.

--- src/obj_printer.h
+++ src/obj_printer.h
@@ -1,8 +1,21 @@
-/* 
- * File:   obj_printer.h
- * Author: Taavi
+/**************************************************************************
  *
- * Created on October 22, 2015, 9:05 AM
+ * GUARDTIME CONFIDENTIAL
+ *
+ * Copyright (C) [2015] Guardtime, Inc
+ * All Rights Reserved
+ *
+ * NOTICE:  All information contained herein is, and remains, the
+ * property of Guardtime Inc and its suppliers, if any.
+ * The intellectual and technical concepts contained herein are
+ * proprietary to Guardtime Inc and its suppliers and may be
+ * covered by U.S. and Foreign Patents and patents in process,
+ * and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this
+ * material is strictly forbidden unless prior written permission
+ * is obtained from Guardtime Inc.
+ * "Guardtime" and "KSI" are trademarks or registered trademarks of
+ * Guardtime Inc.
  */
 
 #ifndef OBJ_PRINTER_H
