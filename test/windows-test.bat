9ff79c7178bfa6b9a770ae3b5f92127bff3196c4
taavi.valjaots <taavi.valjaots@guardtime.com>
2016-05-04 15:18:33 +0300

    TOOL-141: Verification tests for sign tests results implemented.

--- test/windows-test.bat
+++ test/windows-test.bat
@@ -1,2 +1,4 @@
+rmdir /S /Q test\out\sign
 mkdir test\out\sign
 shelltest test\test_suites\sign.test test\test_suites\verify.test --with=bin\ksi.exe -- -j15
+shelltest test\test_suites\sign-verify.test --with=bin\ksi.exe -- -j8
