ea1cbf2f09d1dbb9dbae3207e276ab4af8806dda
Maksim Tamleht <maksim.tamleht@guardtime.com>
2016-05-05 16:59:01 +0300

    Updated verification tests. Using static data.

--- test/windows-test.bat
+++ test/windows-test.bat
@@ -1,4 +1,5 @@
 rmdir /S /Q test\out\sign
 mkdir test\out\sign
-shelltest test\test_suites\sign.test test\test_suites\verify.test --with=bin\ksi.exe -- -j15
-shelltest test\test_suites\sign-verify.test --with=bin\ksi.exe -- -j8
+shelltest test\test_suites\sign.test --with=bin\ksi.exe -p -c -- -j1
+shelltest test\test_suites\sign-verify.test --with=bin\ksi.exe -p -c -- -j1
+shelltest test\test_suites\verify.test --with=bin\ksi.exe -p -c -- -j1
