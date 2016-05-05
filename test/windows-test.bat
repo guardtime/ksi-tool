351448c0441653dba57ab835be40f8593628c1f6
taavi.valjaots <taavi.valjaots@guardtime.com>
2016-05-05 15:42:05 +0300

    TOOL-141: Extend test result verification tests added.

--- test/windows-test.bat
+++ test/windows-test.bat
@@ -3,5 +3,6 @@ RMDIR /S /Q test\out\extend
 mkdir test\out\sign
 mkdir test\out\extend
 
-shelltest test\test_suites\sign.test test\test_suites\verify.test --with=bin\ksi.exe -- -j8
-shelltest test\test_suites\sign-verify.test test\test_suites\extend.test --with=bin\ksi.exe -- -j8
+shelltest test\test_suites\sign.test test\test_suites\verify.test --with=bin\ksi.exe -- -j1
+shelltest test\test_suites\sign-verify.test test\test_suites\extend.test --with=bin\ksi.exe -- -j1
+shelltest test\test_suites\extend-verify.test  --with=bin\ksi.exe -- -j1
