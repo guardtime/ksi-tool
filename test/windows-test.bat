0c5a709a7c7318b790daf078962060125f7f8fd8
Maksim Tamleht <maksim.tamleht@guardtime.com>
2016-05-06 10:11:09 +0300

    Update static verification tests

--- test/windows-test.bat
+++ test/windows-test.bat
@@ -5,5 +5,5 @@ mkdir test\out\extend
 
 shelltest test\test_suites\sign.test --with=bin\ksi.exe -- -j1
 shelltest test\test_suites\sign-verify.test test\test_suites\extend.test --with=bin\ksi.exe -- -j1
-shelltest test\test_suites\extend-verify.test  --with=bin\ksi.exe -- -j1
-shelltest test\test_suites\verify.test --with=bin\ksi.exe -- -j1
+shelltest test\test_suites\extend-verify.test --with=bin\ksi.exe -- -j1
+shelltest test\test_suites\static-verify.test --with=bin\ksi.exe -- -j1
