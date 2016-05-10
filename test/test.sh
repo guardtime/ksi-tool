fae4087c799a0377f0e60162ea068e4c45f3ce35
taavi.valjaots <taavi.valjaots@guardtime.com>
2016-05-10 11:07:34 +0300

    TOOL-141: Script test/test.sh added to run linux tests.

---
+++ test/test.sh
@@ -0,0 +1,15 @@
+
+rm -f test/out/sign 2> /dev/null
+rm -f test/out/extend 2> /dev/null
+mkdir -p test/out/sign
+mkdir -p test/out/extend
+
+shelltest \
+test/test_suites/sign.test \
+test/test_suites/static-sign.test \
+test/test_suites/sign-verify.test \
+test/test_suites/extend.test \
+test/test_suites/extend-verify.test \
+test/test_suites/static-verify.test \
+test/test_suites/static-sign-verify.test \
+--with=src/ksi -- -j1
