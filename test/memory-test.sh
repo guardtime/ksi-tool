601b68490be421b45efc83148241681495ec8030
taavi.valjaots <taavi.valjaots@guardtime.com>
2016-05-27 17:39:38 +0300

    TOOL-141: Memory tests do not skip tests that use stdin.

--- test/memory-test.sh
+++ test/memory-test.sh
@@ -1,52 +1,93 @@
+#!/bin/bash
+
+#
+# GUARDTIME CONFIDENTIAL
+#
+# Copyright (C) [2016] Guardtime, Inc
+# All Rights Reserved
+#
+# NOTICE:  All information contained herein is, and remains, the
+# property of Guardtime Inc and its suppliers, if any.
+# The intellectual and technical concepts contained herein are
+# proprietary to Guardtime Inc and its suppliers and may be
+# covered by U.S. and Foreign Patents and patents in process,
+# and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this
+# material is strictly forbidden unless prior written permission
+# is obtained from Guardtime Inc.
+# "Guardtime" and "KSI" are trademarks or registered trademarks of
+# Guardtime Inc.
+#
+
+mem_test_dir=test/out/memory
+test_suite_dir=test/test_suites
+
 # Remove memory test directory.
-rm -rf test/out/memory 2> /dev/null
+rm -rf $mem_test_dir 2> /dev/null
 
 # Create a temporary output directory for memory tests.
-mkdir -p test/out/memory
+mkdir -p $mem_test_dir
 
-# Configure temporary KSI_CONF .
+# Create some test files to output directory.
+cp test/resource/file/testFile	$mem_test_dir/_
+cp test/resource/file/testFile	$mem_test_dir/10__
+cp test/resource/file/testFile	$mem_test_dir/test_file
+cp test/resource/file/testFile	$mem_test_dir/a_23_500
+cp test/resource/file/testFile	$mem_test_dir/a_23_1000
+cp test/resource/file/testFile	$mem_test_dir/a_23_1000.ksig
+cp test/resource/file/testFile	$mem_test_dir/a_23_1000_5.ksig
+cp test/resource/signature/ok-sig-2014-08-01.1.ksig $mem_test_dir/ok-sig.ksig
+cp test/resource/signature/ok-sig-2014-08-01.1.ksig $mem_test_dir/ok-sig
+
+# Configure temporary KSI_CONF.
 export KSI_CONF=test/resource/conf/default-not-working-conf.cfg
 
-# Convert test files to valgrind memory test files.
-test/convert-to-memory-test.sh test/test_suites/sign.test test/out/memory/sign.test
-test/convert-to-memory-test.sh test/test_suites/static-sign.test test/out/memory/static-sign.test
-test/convert-to-memory-test.sh test/test_suites/sign-verify.test test/out/memory/sign-verify.test
-test/convert-to-memory-test.sh test/test_suites/extend.test test/out/memory/extend.test
-test/convert-to-memory-test.sh test/test_suites/extend-verify.test test/out/memory/extend-verify.test
-test/convert-to-memory-test.sh test/test_suites/static-verify.test test/out/memory/static-verify.test
-test/convert-to-memory-test.sh test/test_suites/static-sign-verify.test test/out/memory/static-sign-verify.test
-test/convert-to-memory-test.sh test/test_suites/static-extend.test test/out/memory/static-extend.test
-test/convert-to-memory-test.sh test/test_suites/sign-cmd.test test/out/memory/sign-cmd.test
-test/convert-to-memory-test.sh test/test_suites/extend-cmd.test  test/out/memory/extend-cmd.test 
-test/convert-to-memory-test.sh test/test_suites/static-verify-invalid-signatures.test  test/out/memory/static-verify-invalid-signatures.test
-test/convert-to-memory-test.sh test/test_suites/pubfile.test  test/out/memory/pubfile.test
-test/convert-to-memory-test.sh test/test_suites/static-pubfile.test  test/out/memory/static-pubfile.test
-test/convert-to-memory-test.sh test/test_suites/verify-invalid-pubfile.test  test/out/memory/verify-invalid-pubfile.test
-test/convert-to-memory-test.sh test/test_suites/verify-cmd.test  test/out/memory/verify-cmd.test
-test/convert-to-memory-test.sh test/test_suites/default-conf.test  test/out/memory/default-conf.test
-test/convert-to-memory-test.sh test/test_suites/invalid-conf.test  test/out/memory/invalid-conf.test
+# A function to convert a test file to memory test.
+function generate_test() {
+test/convert-to-memory-test.sh $test_suite_dir/$1  $mem_test_dir/$1
+}
 
+# Convert test files to valgrind memory test files.
+generate_test sign.test
+generate_test static-sign.test
+generate_test sign-verify.test
+generate_test extend.test
+generate_test extend-verify.test
+generate_test static-verify.test
+generate_test static-sign-verify.test
+generate_test static-extend.test
+generate_test sign-cmd.test
+generate_test extend-cmd.test
+generate_test static-verify-invalid-signatures.test
+generate_test pubfile.test
+generate_test static-pubfile.test
+generate_test verify-invalid-pubfile.test
+generate_test verify-cmd.test
+generate_test default-conf.test
+generate_test invalid-conf.test
+generate_test file-name-gen.test
 
 # Run generated test scripts.
 
 shelltest \
-test/out/memory/sign.test \
-test/out/memory/static-sign.test \
-test/out/memory/sign-verify.test \
-test/out/memory/extend.test \
-test/out/memory/extend-verify.test \
-test/out/memory/static-verify.test \
-test/out/memory/static-sign-verify.test \
-test/out/memory/static-extend.test \
-test/out/memory/sign-cmd.test \
-test/out/memory/extend-cmd.test \
-test/out/memory/static-verify-invalid-signatures.test \
-test/out/memory/pubfile.test \
-test/out/memory/static-pubfile.test \
-test/out/memory/verify-invalid-pubfile.test \
-test/out/memory/verify-cmd.test \
-test/out/memory/default-conf.test \
-test/out/memory/invalid-conf.test \
+$mem_test_dir/sign.test \
+$mem_test_dir/static-sign.test \
+$mem_test_dir/sign-verify.test \
+$mem_test_dir/extend.test \
+$mem_test_dir/extend-verify.test \
+$mem_test_dir/static-verify.test \
+$mem_test_dir/static-sign-verify.test \
+$mem_test_dir/static-extend.test \
+$mem_test_dir/sign-cmd.test \
+$mem_test_dir/extend-cmd.test \
+$mem_test_dir/static-verify-invalid-signatures.test \
+$mem_test_dir/pubfile.test \
+$mem_test_dir/static-pubfile.test \
+$mem_test_dir/verify-invalid-pubfile.test \
+$mem_test_dir/verify-cmd.test \
+$mem_test_dir/default-conf.test \
+$mem_test_dir/invalid-conf.test \
+$mem_test_dir/file-name-gen.test \
 --with="valgrind --leak-check=full src/ksi" -- -j1
 exit_code=$?
 
--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+
+#
+# GUARDTIME CONFIDENTIAL
+#
+# Copyright (C) [2016] Guardtime, Inc
+# All Rights Reserved
+#
+# NOTICE:  All information contained herein is, and remains, the
+# property of Guardtime Inc and its suppliers, if any.
+# The intellectual and technical concepts contained herein are
+# proprietary to Guardtime Inc and its suppliers and may be
+# covered by U.S. and Foreign Patents and patents in process,
+# and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this
+# material is strictly forbidden unless prior written permission
+# is obtained from Guardtime Inc.
+# "Guardtime" and "KSI" are trademarks or registered trademarks of
+# Guardtime Inc.
+#
+
 # Remove test output directories.  
 rm -rf test/out/sign 2> /dev/null
 rm -rf test/out/extend 2> /dev/null
