d6796d64228ac6a1626b63b54057a108d05cdc4b
Maksim Tamleht <maksim.tamleht@guardtime.ee>
2018-09-05 01:38:48 -0400

    Corrected error message.

--- src/tool_box/extend.c
+++ src/tool_box/extend.c
@@ -287,7 +287,7 @@ static int obtain_remote_conf(PARAM_SET *set, ERR_TRCKR *err, KSI_CTX *ctx, size
 
 	print_progressDesc(d, "Receiving remote configuration... ");
 	res = KSITOOL_Extender_getConf(err, ctx, &config);
-	ERR_CATCH_MSG(err, res, "Error: Unable to receive remote config.");
+	ERR_CATCH_MSG(err, res, "Error: Unable to receive remote configuration.");
 	print_progressResult(res);
 
 	if (PARAM_SET_isSetByName(set, "dump-conf")) {
