a9a83bf23221dbe8f49469689194986bc0925c73
taavi.valjaots <taavi.valjaots@guardtime.com>
2019-05-21 12:07:53 +0300

    Memory tests for Windows implemented (test\memory-test.bat).

--- test/memory-test.bat
+++ test/memory-test.bat
@@ -1,3 +1,33 @@
+GOTO copyrightend
+
+    Copyright 2013-2016 Guardtime, Inc.
+
+    This file is part of the Guardtime client SDK.
+
+    Licensed under the Apache License, Version 2.0 (the "License").
+    You may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES, CONDITIONS, OR OTHER LICENSES OF ANY KIND, either
+    express or implied. See the License for the specific language governing
+    permissions and limitations under the License.
+    "Guardtime" and "KSI" are trademarks or registered trademarks of
+    Guardtime, Inc., and no license to trademarks is granted; Guardtime
+    reserves and retains all trademark rights.
+
+:copyrightend
+
+@echo off
+sed --version > NUL
+if not ERRORLEVEL 0 (
+	echo Error:
+	echo   Unable to find program sed. You must have sed to convert regular tests to
+	echo   memory tests.
+	exit /B 1
+)
+
 SETLOCAL
 
 REM Run generated test scripts.
@@ -5,7 +35,6 @@ REM Run generated test scripts.
 SET tool=bin\ksi.exe
 SET mem_test_dir=test\out\memory
 SET test_suite_dir=test\test_suites
-
 
 
 REM Remove memory test directory.
@@ -25,22 +54,29 @@ copy test\resource\file\testFile	%mem_test_dir%\a_23_1000.ksig
 copy test\resource\file\testFile	%mem_test_dir%\a_23_1000_5.ksig
 copy test\resource\signature\ok-sig-2014-08-01.1.ksig %mem_test_dir%\ok-sig.ksig
 copy test\resource\signature\ok-sig-2014-08-01.1.ksig %mem_test_dir%\ok-sig
+copy test\resource\signature\ok-sig-2014-08-01.1.ksig %mem_test_dir%\mass-extend-1.ksig
+copy test\resource\signature\ok-sig-2014-08-01.1.ksig %mem_test_dir%\mass-extend-2.ksig
+copy test\resource\signature\ok-sig-2014-08-01.1.ksig %mem_test_dir%\mass-extend-2.ksig
+copy test\resource\signature\ok-sig-2014-08-01.1.ksig %mem_test_dir%\not-extended-1A.ksig
+copy test\resource\signature\ok-sig-2014-08-01.1.ksig %mem_test_dir%\not-extended-1B.ksig
+copy test\resource\signature\ok-sig-2014-08-01.1.ksig %mem_test_dir%\not-extended-2B.ksig
 
 REM  Configure temporary KSI_CONF.
-SET KSI_CONF=test\resource\conf\default-not-working-conf.cfg
+SET KSI_CONF=test/resource/conf/default-conf.cfg
 
 
-
- REM Convert test files to valgrind memory test files.
+REM Convert test files to valgrind memory test files.
 call test\convert-to-memory-test.bat %test_suite_dir%\sign.test %mem_test_dir%\sign.test
 call test\convert-to-memory-test.bat %test_suite_dir%\static-sign.test %mem_test_dir%\static-sign.test
 call test\convert-to-memory-test.bat %test_suite_dir%\sign-verify.test %mem_test_dir%\sign-verify.test
 call test\convert-to-memory-test.bat %test_suite_dir%\extend.test %mem_test_dir%\extend.test
+call test\convert-to-memory-test.bat %test_suite_dir%\mass-extend.test %mem_test_dir%\mass-extend.test
 call test\convert-to-memory-test.bat %test_suite_dir%\extend-verify.test %mem_test_dir%\extend-verify.test
 call test\convert-to-memory-test.bat %test_suite_dir%\static-verify.test %mem_test_dir%\static-verify.test
 call test\convert-to-memory-test.bat %test_suite_dir%\static-sign-verify.test %mem_test_dir%\static-sign-verify.test
 call test\convert-to-memory-test.bat %test_suite_dir%\static-extend.test %mem_test_dir%\static-extend.test
 call test\convert-to-memory-test.bat %test_suite_dir%\sign-cmd.test %mem_test_dir%\sign-cmd.test
+call test\convert-to-memory-test.bat %test_suite_dir%\signature-dump.test %mem_test_dir%\signature-dump.test
 call test\convert-to-memory-test.bat %test_suite_dir%\extend-cmd.test %mem_test_dir%\extend-cmd.test
 call test\convert-to-memory-test.bat %test_suite_dir%\static-verify-invalid-signatures.test %mem_test_dir%\static-verify-invalid-signatures.test
 call test\convert-to-memory-test.bat %test_suite_dir%\pubfile.test %mem_test_dir%\pubfile.test
@@ -50,19 +86,25 @@ call test\convert-to-memory-test.bat %test_suite_dir%\verify-cmd.test %mem_test_
 call test\convert-to-memory-test.bat %test_suite_dir%\default-conf.test %mem_test_dir%\default-conf.test
 call test\convert-to-memory-test.bat %test_suite_dir%\invalid-conf.test %mem_test_dir%\invalid-conf.test
 call test\convert-to-memory-test.bat %test_suite_dir%\file-name-gen.test %mem_test_dir%\file-name-gen.test
+call test\convert-to-memory-test.bat %test_suite_dir%\cmd.test %mem_test_dir%\cmd.test
 call test\convert-to-memory-test.bat %test_suite_dir%\sign-block-signer.test %mem_test_dir%\sign-block-signer.test
 call test\convert-to-memory-test.bat %test_suite_dir%\sign-block-signer-cmd.test %mem_test_dir%\sign-block-signer-cmd.test
+call test\convert-to-memory-test.bat %test_suite_dir%\verify-pub-suggestions.test %mem_test_dir%\verify-pub-suggestions.test
+
+@echo on
 
 shelltest -a ^
 %mem_test_dir%\sign.test ^
 %mem_test_dir%\static-sign.test ^
 %mem_test_dir%\sign-verify.test ^
 %mem_test_dir%\extend.test ^
+%mem_test_dir%\mass-extend.test ^
 %mem_test_dir%\extend-verify.test ^
 %mem_test_dir%\static-verify.test ^
 %mem_test_dir%\static-sign-verify.test ^
 %mem_test_dir%\static-extend.test ^
 %mem_test_dir%\sign-cmd.test ^
+%mem_test_dir%\signature-dump.test ^
 %mem_test_dir%\extend-cmd.test ^
 %mem_test_dir%\static-verify-invalid-signatures.test ^
 %mem_test_dir%\pubfile.test ^
@@ -72,8 +114,10 @@ shelltest -a ^
 %mem_test_dir%\default-conf.test ^
 %mem_test_dir%\invalid-conf.test ^
 %mem_test_dir%\file-name-gen.test ^
+%mem_test_dir%\cmd.test ^
 %mem_test_dir%\sign-block-signer.test ^
 %mem_test_dir%\sign-block-signer-cmd.test ^
+%mem_test_dir%\verify-pub-suggestions.test ^
 --with="drmemory -leaks_only -logdir %mem_test_dir%\dr_memory -report_leak_max -1 -batch -- %tool%" -- -j1
 
 
